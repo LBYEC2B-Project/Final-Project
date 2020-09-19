5befbecba28132386025db4d63127896708a0a22
DarrenceTanhuy <68207523+DarrenceTanhuy@users.noreply.github.com>
2020-09-19 23:39:58 +0800

    Add files via upload

--- InventoryClassifier.cpp
+++ InventoryClassifier.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,34 +8,55 @@ class BasicInfo{
     char name[20], company[20];
     public:
     BasicInfo(){
-        cout<<"Please Enter Basic Information\n";
+        cout<<"Basic User Information\n";
         cout<<"Name of User: ";
         cin>>name;
         cout<<"Company Name: ";
         cin>>company;
     };
 };
+class ProductInfo{
+    private: 
+    char name[20], classification[20];
+    int quantity;
+    float price;
+    public:
+    void productdetails(){
+        cout<<"Product Details\n";
+        cout<<"Classification: ";
+        cin>>classification;
+        cout<<"Name: ";
+        cin>>name;
+        cout<<"Price: ";
+        cin>>price;
+        cout<<"Quantity: ";
+        cin>>quantity;
+    };
+};
 
 class AddNewProducts{
     private: 
-    char name[20], classification[20];
-    int quantity, price;
+//code for adding products
     public: 
-
 };
 
-class ViewCurrentList{
-
+class ViewCurrentList{ 
+    private: 
+    
+    public: 
+//code for reading txt file
 };
 
 class EditList{
-
+    private: 
+// code for editing list
+    public: 
 };
 
 int main(){
     int x, y;
     cout<<"|||Product Inventory System v1.0|||\n\n";
-    BasicInfo info1;
+    BasicInfo info;
     Options:
     cout<<"\nProgram Functions";
     cout<<"\n1) Add Product(s)\n2) View Current Inventory Listing\n3) Edit Inventory Listing\n4) Exit Program\n\n";
@@ -63,29 +85,8 @@ int main(){
         cout<<"ERROR: Please choose from options 1, 2, or 3. Try again\n\n";
         goto Options;
         };
+    //code for writing to txt file, could be at main or in a class
     };
-    Options2:
-    cout<<"Would you like to return to the menu?\n";
-    cout<<"1) Yes\n2) No\n";
-    cout<<"Input Option: ";
-    cin>>x;
-    switch(y){
-        case 1:
-        {
-            goto Options;
-            break;
-        }
-        case 2:
-        {
-            cout<<"\nThank you!\n";
-            return 0;
-        }
-        default:
-        {
-        cout<<"ERROR: Please choose from options 1 or 2. Try again\n\n";
-        goto Options2;
-        };
-    };
+    goto Options;
     return 0;
 };
-
